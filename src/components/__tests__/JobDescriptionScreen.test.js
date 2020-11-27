0c9d71db01163ad1e0b2cd7dbb38378e9cbfa94c
Federico Valles <fedeva_37@hotmail.com>
2020-11-27 18:20:31 -0300

    Snapshot test added

--- src/components/__tests__/JobDescriptionScreen.test.js
+++ src/components/__tests__/JobDescriptionScreen.test.js
@@ -4,9 +4,13 @@ import renderer from 'react-test-renderer';
 import JobDescriptionScreen from '../JobDescriptionScreen';
 
 describe('<JobDescriptionScreen />', () => {
+  const route = { params: { jobDesc: 'Mock Job Description' } };
   it('has 1 child', () => {
-    const route = { params: { jobDesc: 'Mock Job Description' } };
     const tree = renderer.create(<JobDescriptionScreen route={route} />).toJSON();
     expect(tree.children.length).toBe(1);
+  });
+  it('renders correctly', () => {
+    const tree = renderer.create(<JobDescriptionScreen route={route} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
