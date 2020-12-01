88d315d72e2cd29d44dbfed6d879ca773b3b4206
Federico Valles <fedeva_37@hotmail.com>
2020-11-30 21:19:07 -0300

    Unit tests corrected

--- src/screens/__tests__/JobDescriptionScreen.test.js
+++ src/screens/__tests__/JobDescriptionScreen.test.js
@@ -4,7 +4,14 @@ import renderer from 'react-test-renderer';
 import JobDescriptionScreen from '../JobDescriptionScreen';
 
 describe('<JobDescriptionScreen />', () => {
-  const route = { params: { jobDesc: 'Mock Job Description' } };
+  const route = {
+    params: {
+      jobDesc: 'Mock Job Description',
+      companyName: 'Mock Company Name',
+      jobId: 'Mock Job ID',
+      jobTitle: 'Mock Job Title',
+    },
+  };
   it('has 1 child', () => {
     const tree = renderer.create(<JobDescriptionScreen route={route} />).toJSON();
     expect(tree.children.length).toBe(1);
